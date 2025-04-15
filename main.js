4b647b6c1c3573c264e565c509e093ada54ccb79
ErikaZanin <erikaczbu@gmail.com>
2025-04-15 17:24:24 -0300

    Atualizando codigo

--- main.js
+++ main.js
@@ -24,21 +24,21 @@ window.addEventListener('scroll', () => {
 
 
 
-  document.querySelectorAll('.nav-item').forEach(item => {
+document.querySelectorAll('.nav-item').forEach(item => {
     item.addEventListener('click', function () {
-      const link = item.querySelector('.nav-link');
-      const targetId = link.getAttribute('href');
-      const targetElement = document.querySelector(targetId);
+        const link = item.querySelector('.nav-link');
+        const targetId = link.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
 
-      if (targetElement) {
-        // Impede o comportamento padrão do link
-        event.preventDefault();
+        if (targetElement) {
+            // Impede o comportamento padrão do link
+            event.preventDefault();
 
-        // Scroll suave até o elemento alvo
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-      }
+            // Scroll suave até o elemento alvo
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
     });
-  });
+});
 
 // Supondo que você tenha uma lista de valores para calcular a média
 const valores = [100, 200, 300, 350]; // Substitua com os valores reais
@@ -48,24 +48,24 @@ const media = valores.reduce((total, num) => total + num, 0) / valores.length;
 
 // Só faz a alternância do display se a média for menor que 370
 if (media < 370) {
-  document.querySelectorAll('.btnList').forEach(item => {
-    item.addEventListener('click', function () {
-      const navbar = document.querySelector('.navbar');
-      const section = document.querySelector('section'); 
-      
-      // Verifica se o navbar existe e alterna o display
-      if (navbar) {
-        if (navbar.style.display === 'flex') {
-          navbar.style.display = 'none'; 
-          section.style.marginTop = '40px';
-        } else {
-          navbar.style.display = 'flex'; 
-          section.style.marginTop = '165px'; 
-        }
-      }
+    document.querySelectorAll('.btnList').forEach(item => {
+        item.addEventListener('click', function () {
+            const navbar = document.querySelector('.navbar');
+            const section = document.querySelector('section');
+
+            // Verifica se o navbar existe e alterna o display
+            if (navbar) {
+                if (navbar.style.display === 'flex') {
+                    navbar.style.display = 'none';
+                    section.style.marginTop = '40px';
+                } else {
+                    navbar.style.display = 'flex';
+                    section.style.marginTop = '165px';
+                }
+            }
+        });
     });
-  });
 }
 
-  
-  
+
+
